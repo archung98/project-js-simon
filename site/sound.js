abda8e1b9291d87309b32acb12d0f9d1bd22cb5a
Ariel Urim Chung <arielurimc_@Ariels-MacBook-Pro.local>
2020-06-11 19:50:09 +0900

    sound?

---
+++ site/sound.js
@@ -0,0 +1,8 @@
+var bluebleep = new Audio ();
+bluebleep.src = "bluesound.wav";  //what if sound track is different for each color?
+var redbleep = new Audio ();
+redbleep.src = "redsound.wav";
+var yellowbleep = new Audio ();
+yellowbleep.src = "yellowsound.wav";
+var greenbleep = new Audio();
+greenbleep.src = "greensound.wav";
