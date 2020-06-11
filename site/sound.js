1207af61441bf8201b6975aa2459c151fe6caf10
Ariel Urim Chung <arielurimc_@Ariels-MacBook-Pro.local>
2020-06-11 23:00:43 +0900

    audioworks

--- site/sound.js
+++ site/sound.js
@@ -1,8 +1,28 @@
+
+/*
 var bluebleep = new Audio ();
-bluebleep.src = "bluesound.wav";  //what if sound track is different for each color?
+bluebleep.src = "bluesound.wav";  
 var redbleep = new Audio ();
 redbleep.src = "redsound.wav";
 var yellowbleep = new Audio ();
 yellowbleep.src = "yellowsound.wav";
 var greenbleep = new Audio();
 greenbleep.src = "greensound.wav";
+*/
+
+
+// $('.simon-button.green').on('click',function(){
+//     $("audio").append("greensound.wav");
+//     // var aud = document.createElement("audio");
+//     // aud.src = "greensound.wav";
+//     // aud.play();
+// });
+
+var bluesound = new Audio();
+bluesound.src  = "audio/bluesound.wav";
+var greensound = new Audio();
+greensound.src = "audio/greensound.wav";
+var redsound = new Audio();
+redsound.src = "audio/redsound.wav";
+var yellowsound = new Audio();
+yellowsound.src = "audio/yellowsound.wav";
