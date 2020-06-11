b4885795321d015e66dd7a409f89036081ff84c0
Pranjal <mahesh@u58233-Pranjals-MacBook-Pro.local>
2020-06-11 23:55:12 +0530

    done till counter

--- site/simon.js
+++ site/simon.js
@@ -1,2 +1,17 @@
-// This file contains the game logic.
-// All the event-listening should happen in buttons.js
+
+ var counter = 0
+function clickcounter(){
+    counter += 1;
+    document.getElementById("clicks").innerHTML = counter;   //this line calls the <h2> by the id and adds to that
+}
+
+
+
+function userresponse (){
+    document.getElementById("colortext").innerHTML = "RED";
+    
+    //maybe write an if statement to check if .simon-button.red has the class light up?? A problem: but light up is removed 
+    // maybe add a var that is True when red is clicked and False when another is clicked; needs to be in the global scope
+    //check if that variable is True of False  to see if it will be GAME OVER or moves on to the next color 
+
+}
